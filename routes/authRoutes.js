85458442d45d493d31f3ec41d040b36c0924ae42
Ashley Hardin <ashleyhardin@Ashleys-MacBook-Air.local>
2018-03-08 09:46:18 -0500

    server with basic google authentication and login routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  )
+  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get('/auth-or-not',checkAuthentication,function(req,res){
+    //do something only if user is authenticated
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logOut();
+    res.send(req.user);
+  });
+};
+
+function checkAuthentication(req,res,next){
+  if(req.isAuthenticated()){
+      //if user is looged in, req.isAuthenticated() will return true 
+      console.log("authenticated")
+      next();
+  } else{
+      res.redirect("/login");
+  }
+}
